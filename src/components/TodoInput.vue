d471fff6e455c0e90d0ff7b05baaf14ca7eabd50
이승환 <lsh01087674117@gmail.com>
2020-05-03 20:00:49 +0900

    모달 컴포넌트 등록

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,25 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="fas fa-plus addBtn"></i>
     </span>
+
+    <Modal v-if="showModal" @close="showModal = false">
+      <!--
+      you can use custom content here to overwrite
+      default content
+      -->
+      <h3 slot="header">custom header</h3>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from "./commom/Modal.vue";
+
 export default {
   data: function() {
     return {
-      newTodoItem: ""
+      newTodoItem: "",
+      showModal: false
     };
   },
   methods: {
@@ -24,6 +35,9 @@ export default {
     clearInput: function() {
       this.newTodoItem = "";
     }
+  },
+  components: {
+    Modal: Modal
   }
 };
 </script>
