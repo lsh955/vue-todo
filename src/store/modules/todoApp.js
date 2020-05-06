0902fb4c12870198f48508d70cc6697add6b568a
Lee Seunghwa <lshk955@naver.com>
2020-05-06 15:48:02 +0900

    [리팩토링] 스토어 모듈화2

---
+++ src/store/modules/todoApp.js
@@ -0,0 +1,58 @@
+const storage = {
+     fetch() {
+       const arr = [];
+       if (localStorage.length > 0) {
+         for (let i = 0; i < localStorage.length; i++) {
+           if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+             arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+           }
+         }
+       }
+       return arr;
+     },
+   };
+
+const state = {      // 프로젝트에서 공통으로 사용할 변수를 정의(여러 컴포넌트에 공유되는 데이터)
+     todoItems: storage.fetch()
+};
+
+const getters = {
+     storedTodoItems(state) {
+          return state.todoItems;
+     }
+};
+
+const mutations = {
+     // state를 변경시키는 역활(Mutations을 통해서만 state를 변경해야 한다.)
+     // Mutitions내에 있는 함수의 인자는 state와 payload
+     // 기본 인자는 state 사용 commit으로 넘어온 전달 인자는 payload 사용
+     // payload는 여러개를 묶은 객체 형태로 전달 될 수 있다.
+     addOneItem(state, todoItem) { 
+          const obj = { completed: false, item: todoItem };
+          localStorage.setItem(todoItem, JSON.stringify(obj));
+          state.todoItems.push(obj);
+     },
+     removeOneItem(state, payload) {
+          localStorage.removeItem(payload.todoItem.item);
+          state.todoItems.splice(payload.index, 1);
+     },
+     toggleOneItem(state, payload) {
+          state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+          // 로컬 스토리지의 데이터를 갱신
+          localStorage.removeItem(payload.todoItem.item);
+          localStorage.setItem(
+               payload.todoItem.item,
+               JSON.stringify(payload.todoItem)
+          );
+     },
+     clearAllItems(state) {
+          localStorage.clear();
+          state.todoItems = [];
+     },
+};
+
+export default { 
+     state,
+     getters,
+     mutations
+ }
